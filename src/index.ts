30ec264314f8ba9e6b81bc1d4a7215d836eaffb3
Yosshy <Yosshy_123@proton.me>
2026-01-17 05:32:34 +0900

    軽微な修正

--- src/index.ts
+++ src/index.ts
@@ -355,7 +355,7 @@ function bookmarkIconSvg() {
 
 function userIconSvg() {
   return `
-    <g transform="translate(0,-4)">
+    <g transform="translate(0,-3)">
       <circle cx="12" cy="8" r="3" stroke="currentColor" stroke-width="1.2" fill="none"/>
       <path d="M4 20c1.5-3 5-5 8-5s6.5 2 8 5" stroke="currentColor" stroke-width="1.2" fill="none" stroke-linecap="round"/>
     </g>
