157e51044cd8e7cbb2ed685e93e1affbeb691766
Yosshy <Yosshy_123@proton.me>
2026-01-17 10:23:33 +0900

    デザインバグの修正

--- src/index.ts
+++ src/index.ts
@@ -303,7 +303,7 @@ function createStatBox(
 ) {
   return `
   <g transform="translate(${x},${y})">
-    <rect x="0" y="0" width="86" height="48" rx="8" />
+    <rect x="0" y="0" width="86" height="48" rx="8" fill="#fbfdff"/>
 
     <svg
       x="10"
